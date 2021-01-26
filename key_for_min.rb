46cace3b3a28384d93743c914738a635cddf7abc
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-26 19:17:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,17 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-#def key_for_min_value(name_hash)
-  #smalls = 1,000,000
-  #lil_key = nil
-  #name_hash.each do |key, value|
-    #if key[value].class == Integer && key[value] < smalls
-     # smalls = value
-      #lil_key = key
-  #end
-#end
-  lil_key
-#end
+
 
 def key_for_min_value(name_hash)
   value_to_compare = 100000000
