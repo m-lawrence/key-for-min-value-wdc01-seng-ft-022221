945c22045ae806ba188f755d12c898826d85d869
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-26 18:54:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   smalls = 1,000,000
   name_hash.each do |key, value|
-    if value.is_a?(Integer) && value < smalls
+    if value.class == Integer && value < smalls
       smalls = key
   end
 end
